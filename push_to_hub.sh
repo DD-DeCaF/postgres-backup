fff546d90712bf0329d0dc31308193d5e4ac76b8
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-10 15:41:36 +0100

    adapt push script to corrected version

--- push_to_hub.sh
+++ push_to_hub.sh
@@ -4,13 +4,13 @@ set -ev
 
 REPO="dddecaf/postgres-backup"
 GIT_MASTER_HEAD_SHA=$(git rev-parse --short=12 --verify HEAD)
-BRANCH=$(git symbolic-ref --short HEAD)
+BRANCH=${TRAVIS_BRANCH:=$(git symbolic-ref --short HEAD)}
 
 docker build -f Dockerfile -t $REPO:$BRANCH .
 docker tag $REPO:$BRANCH $REPO:$GIT_MASTER_HEAD_SHA
 docker push $REPO:$BRANCH
 docker push $REPO:$GIT_MASTER_HEAD_SHA
-if [ BRANCH = "master" ]; then
+if [ $BRANCH = "master" ]; then
    docker tag $REPO:$BRANCH $REPO:latest
    docker push $REPO:latest
 fi
